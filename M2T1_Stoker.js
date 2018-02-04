eb39cf85df7a2fe49523a5d8834be0657dc195af
WinStoker <31968363+WinStoker@users.noreply.github.com>
2018-02-04 14:26:15 -0500

    Add files via upload

---
+++ M2T1_Stoker.js
@@ -0,0 +1,55 @@
+//Question 1
+console.log('mushrooms');
+//Question 2 
+console.log('calvin & hobbes')
+
+//Question 1
+console.log('JavaScript')
+//Question 2
+console.log(33.7)
+//Question 3
+
+//Question 1
+console.log(true)
+//Question 2
+console.log(null)
+
+//Question 1
+console.log(3.5 + 28);
+//Question 2
+console.log(2018 - 1969);
+//Question 3
+console.log(65 / 240);
+//Question 4 
+console.log(0.2708 * 100);
+
+//Question 1
+console.log('Teaching the world how to code'.length);
+
+//Question 1
+// Log Codecademy in all uppercase letters
+console.log('Codecademy'.toUpperCase()); 
+//Question 2
+// Use a string method to log the following statment without whitespace at the beginning and end of it.
+console.log('    Remove whitespace   '.trim()); 
+
+//Question 1
+console.log(Math.random() * 100);
+//Question 2
+console.log(Math.floor(Math.random() * 100));
+//Question 3
+console.log(Math.ceil(43.8));
+//Question 4
+console.log(Number.isInteger(2017));
+
+//Question 1
+//Opening line
+console.log('It was love at first sight.');
+//Question 2
+/*console.log('The first time Yossarian saw the chaplain he fell madly in love with him.');
+console.log('Yossarian was in the hospital with a pain in his liver that fell just short of being jaundice.');
+console.log('The doctors were puzzled by the fact that it wasn\'t quite jaundice.');
+console.log('If it became jaundice they could treat it.');
+console.log('If it didn\'t become jaundice and went away they could discharge him.');
+console.log('But this just being short of jaundice all the time confused them.');*/
+
